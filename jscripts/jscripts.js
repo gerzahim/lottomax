c2e6fe1822706ad138425adfe55204e1c25349db
rasce88 <rasce88@e35f22ce-04d5-d3c6-2b86-22119a932780>
2014-02-12 16:35:06 +0000

    arreglando peito de ventas y accesos rapidos

--- jscripts/jscripts.js
+++ jscripts/jscripts.js
@@ -1048,12 +1048,12 @@ $(document).keyup(function(tecla){
 function ValidateFormSell(){
 	jQuery('#txt_numero').keydown(function (event) {
 	    // Allow only backspace and delete
-	           if (event.keyCode == 46 || event.keyCode == 8  || event.keyCode == 9 || event.keyCode == 110 || event.keyCode == 109 || (event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105)) {
+	           if (event.keyCode == 127 || event.keyCode == 46 || event.keyCode == 8  || event.keyCode == 9 || event.keyCode == 110 || event.keyCode == 109 || (event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105)) {
 	        	   //alert(event.keyCode);
 	        	   // let it happen, don't do anything
 	        	   // 46 = punto ; 8 = Retroceso ; 9 = tabulador
 	        	   // 106 = asterisco; 109 = menos para corrida
-	        	   // 110 punto; 16 = shift; 32= space
+	        	   // 110 punto; 16 = shift; 32= space; 127= delete
 	        	   
 	               // Ensure that it is a number and stop the keypress
 	               if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105)) {
@@ -1078,10 +1078,11 @@ function ValidateFormSell(){
 	                 if(count > 6 && juego == 4) {
                 		 event.preventDefault();
                 	 }                 
-	               }
+	               }/*
 	               else {
+	            	   //alert('puta');
 	                   event.preventDefault();
-	               }    	   
+	               } */   	   
 	           }
 	           else {
 	        	   //alert(event.keyCode);
