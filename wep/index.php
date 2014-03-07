ea2a32f9a6dc7c300cbfb1b8d78b51b9042c70d0
rasce88 <rasce88@e35f22ce-04d5-d3c6-2b86-22119a932780>
2014-03-07 19:13:10 +0000

    jscripts y wep 2

---
+++ wep/index.php
@@ -0,0 +1,28 @@
+<html>
+<body>
+<table width="205" border="0">
+  <tr>
+    <th width="195" scope="col">Lottomax Movil</th>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td ><a href="CargarResultados.php" target="_blank">Cargar Resultados</a></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><a href="ReporteBanca.php" target="_blank">Reporte Banca</a></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><a href="TicketsGanadoresDetalle.php" target="_blank">Detalle Tickets Ganadores</a></td>
+  </tr>  
+</table>
+
+</body>
+</html>
