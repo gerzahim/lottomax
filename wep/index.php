4f1033acf30c5f429110992f6c7e547e063b1d99
rasce88 <rasce88@e35f22ce-04d5-d3c6-2b86-22119a932780>
2014-03-08 00:14:58 +0000

    Lottomax Movil

--- wep/index.php
+++ wep/index.php
@@ -7,6 +7,14 @@
   <tr>
     <td>&nbsp;</td>
   </tr>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>    
   <tr>
     <td ><a href="CargarResultados.php" target="_blank">Cargar Resultados</a></td>
   </tr>
@@ -14,12 +22,28 @@
     <td>&nbsp;</td>
   </tr>
   <tr>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>    
     <td><a href="ReporteBanca.php" target="_blank">Reporte Banca</a></td>
   </tr>
   <tr>
     <td>&nbsp;</td>
   </tr>
   <tr>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>  
     <td><a href="TicketsGanadoresDetalle.php" target="_blank">Detalle Tickets Ganadores</a></td>
   </tr>  
 </table>
