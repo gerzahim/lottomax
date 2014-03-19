81cda94830fd7f3b0b67bf7304ab80f572720a13
rasce88 <rasce88@e35f22ce-04d5-d3c6-2b86-22119a932780>
2014-03-19 22:25:47 +0000

    Cambios ImprimirReporteBanca.php

--- ajax/ImprimirReporteBanca.php
+++ ajax/ImprimirReporteBanca.php
@@ -48,6 +48,27 @@ session_start();
 // Obtenemos los datos de la taquilla
 $id_taquilla= $obj_modelo->GetIdTaquilla();
 
+$string=date('Y-m-d H:i:s');
+$year = substr($string,0,4);
+$month = substr($string,5,2);
+$day = substr($string,8,2);
+$hour = substr($string,11,2);
+$minute = substr($string,14,2);
+$dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado");
+//$fecha_hora=$day."-".$month."-".$year." ".$hour.":".$minute;
+$fecha_hora=$day."-".$month."-".$year;
+
+if($hour > 11){
+	$formato="PM";
+}else{
+	$formato="AM";
+}
+$formato_militar= array("13","14","15","16","17","18","19","20","21","22","23","24");
+$formato_civil= array("01","02","03","04","05","06","07","08","09","10","11","12");
+
+$hour= str_replace($formato_militar,$formato_civil,$hour);
+$hora=$hour.":".$minute." ".$formato;
+
 
 $fecha_desde= $obj_generico->CleanText($_GET['fechadesde']);
 $fecha_hasta= $obj_generico->CleanText($_GET['fechahasta']);
@@ -63,6 +84,9 @@ $data="";
 				// ENCABEZADO DEL TICKET
 				
             	$data.=" <table width='100%' border='0' ><tr><td colspan='3' align='center'><font face='arial' size='2' >";
+            	$data.=$dias[date('w')]." ".$fecha_hora."&nbsp;&nbsp;".$hora;
+            	$data.="<br>";
+            	$data.="<br>";
             	$data.=" SISTEMA LOTTOMAX";
             	$data.=" </font></td> </tr>";
 
@@ -87,6 +111,11 @@ $data="";
 				$data1.="CUADRE CON BANCA";
 				$data1.="\\n";										            
               */
+
+            	$total_venta=0;
+            	$total_comision=0;
+            	$total_premios=0;
+            	$total_balance=0;
                 while($row= $obj_conexion->GetArrayInfo($result)){
                         
 					
@@ -95,22 +124,23 @@ $data="";
                 	
                 	$data.="<tr><td width='50%' align='left' > <font face='times' size='2' > Total de Ventas: </font></td> ";
                 	$data.="<td width='50%' align='center'><font face='times' size='2' > ".round($row['total_ventas'], 2)." Bs.</font></td></tr>";
-                	 
+                	$total_venta= $total_venta + round($row['total_ventas'], 2);
 
                 	$data.="<tr><td width='50%' align='left'> <font face='times' size='2' > Comisi&oacute;n: </font></td> ";
                 	$data.="<td width='50%' align='center'><font face='times' size='2' > ".round($row['comision'], 2)." Bs.</font></td></tr>";
-                	
+                	$total_comision= $total_comision + round($row['comision'], 2);
 
                 	$data.="<tr><td width='50%' align='left'> <font face='times' size='2' > Total Premios: </font></td> ";
                 	$data.="<td width='50%' align='center'><font face='times' size='2' > ".round($row['total_premiado'], 2)." Bs.</font></td></tr>";
+                	$total_premios= $total_premios + round($row['total_premiado'], 2);
                 	
-                	$data.="<tr height='2'><td colspan='2' align='center'></td></tr>";
+                	
                 	 
                 	$data.="<tr><td width='40' align='left'> <font face='times' size='2' > Balance: </font></td> ";
                 	$data.="<td width='50%' align='center'><font face='times' size='4' > ".round($row['balance'], 2)." Bs.</font></td></tr>";
-                	 
+                	$total_balance= $total_balance + round($row['balance'], 2);
 					
-					
+                	$data.="<tr height='2'><td colspan='2' align='center'>&nbsp;</td></tr>";
 					/*$data1.="\\nFECHA: ".$row['fecha'];
 					$data1.="\\nTotal Ventas: ".$row['total_ventas'];					                									
 					$data1.="\\nComision: ".$row['comision'];
@@ -122,7 +152,11 @@ $data="";
 				      */                  
                     
                 }
- 				
+                $data.="<tr height='10'><td colspan='2' align='center'>===========================</td></tr>";
+                $data.="<tr height='10'><td colspan='2' align='left'>Total Venta: ".$total_venta." Bs:</td></tr>";
+                $data.="<tr height='10'><td colspan='2' align='left'>Total Comision: ".$total_comision." Bs:</td></tr>";
+                $data.="<tr height='10'><td colspan='2' align='left'>Total Premios: ".$total_premios." Bs:</td></tr>";
+                $data.="<tr height='10'><td colspan='2' align='left'>Total Balance: ".$total_balance." Bs:</td></tr>";
                 //echo $data;               
             } 
 
